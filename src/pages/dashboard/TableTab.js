d88a616acd8f3e63f204cb9bc20144b13e37b456
kitpasin <kitpasin.ice@gmail.com>
2023-05-25 12:59:08 +0700

    review order

--- src/pages/dashboard/TableTab.js
+++ src/pages/dashboard/TableTab.js
@@ -9,6 +9,8 @@ import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Modal } from "@mui/material/";
+import { useState } from "react";
 
 const headCells = [
   {
@@ -88,8 +90,36 @@ export default function TableTab({ orderList }) {
   };
 
   // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - orderList.length) : 0;
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - orderList.length) : 0;
+
+  console.log(orderList);
+
+  const [selectedOrdersNumber, setSelectedOrdersNumber] = useState(null);
+  const [selectedProductsName, setSelectedProductsName] = useState([]);
+  const [selectedTypesOrder, setSelectedTypesOrder] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleTableRowClick = (ordersNumber, productsName, typesOrder) => {
+    setSelectedOrdersNumber(ordersNumber);
+    setSelectedProductsName(productsName);
+    setSelectedTypesOrder(typesOrder);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  let productWashingCap, productWashingTemp, productDryingCap, productFoods;
+
+  if (selectedProductsName && typeof selectedProductsName === "string") {
+    const productsArray = selectedProductsName.split(",");
+    productWashingCap = productsArray[0];
+    productWashingTemp = productsArray[1];
+    productDryingCap = productsArray[2];
+    productFoods = selectedProductsName.replace(/,/g, ", ")
+  }
+
 
   return (
     <Box sx={{ width: "100%" }}>
@@ -104,21 +134,24 @@ export default function TableTab({ orderList }) {
             <TableBody>
               {orderList?.map((row, index) => {
                 return (
-                  <TableRow tabIndex={-1} key={index}>
-                    <TableCell
-                      component="th"
-                      scope="row"
-                      padding="normal"
-                    >
-                      {row.type_order}
+                  <TableRow
+                    tabIndex={-1}
+                    key={index}
+                    onClick={() =>
+                      handleTableRowClick(row.orders_number, row.product_name, row.type_order)
+                    }
+                    style={{cursor: "pointer"}}
+                  >
+                    <TableCell component="th" scope="row" padding="normal">
+                      {row.type_order === "washing" ? "Washing and Drying" : "Foods"}
                     </TableCell>
                     <TableCell align="left">{row.transaction_date}</TableCell>
                     <TableCell align="left">{row.delivery_price} THB</TableCell>
                     <TableCell align="left">{row.total_price} THB</TableCell>
-                    <TableCell align="left">
-                      {row.total_price + row.delivery_price} THB
+                    <TableCell align="left">{row.total_price + row.delivery_price} THB</TableCell>
+                    <TableCell align="left" style={{ textTransform: "capitalize" }}>
+                      {row.type_payment}
                     </TableCell>
-                    <TableCell align="left">{row.type_payment}</TableCell>
                     <TableCell
                       align="left"
                       style={{
@@ -142,6 +175,65 @@ export default function TableTab({ orderList }) {
                 </TableRow>
               )}
             </TableBody>
+            <Modal
+              open={isModalOpen}
+              onClose={handleCloseModal}
+              aria-labelledby="modal-title"
+              aria-describedby="modal-description"
+            >
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  bgcolor: "background.paper",
+                  boxShadow: 24,
+                  p: 4,
+                  outline: "none",
+                  maxWidth: 768,
+                  borderRadius: "10px",
+                }}
+              >
+                <div
+                  id="modal-title"
+                  style={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    alignItems: "center",
+                    gap: "0.25rem",
+                    fontSize: "20px",
+                  }}
+                >
+                  <p style={{ fontWeight: "bold" }}>Orders Number :</p>
+                  <p>{selectedOrdersNumber}</p>
+                </div>
+                <div
+                  id="modal-description"
+                  style={{
+                    display: "flex",
+                    justifyContent: "flex-start",
+                    alignItems: "flex-start",
+                    gap: "0.25rem",
+                    fontSize: "20px",
+                    width: "100%",
+                  }}
+                >
+                  <div>
+                    <p style={{ fontWeight: "bold" }}>Product : </p>
+                  </div>
+                  <div>
+                    {selectedTypesOrder === "washing" ? (
+                      <p>
+                        Washing {productWashingCap} {productWashingTemp}, Drying {productDryingCap}
+                      </p>
+                    ) : (
+                      <p>{productFoods}</p>
+                    )}
+                  </div>
+                </div>
+              </Box>
+            </Modal>
           </Table>
         </TableContainer>
         <TablePagination
